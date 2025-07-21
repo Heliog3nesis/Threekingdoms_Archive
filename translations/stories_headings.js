29a891809de8edf59c3bfdc31a2a60b0305a2719
Heliog3nesis <zyugongjin@gmail.com>
2025-07-21 10:55:09 +0800

    Added tooltip for maps, added short stories collection

---
+++ translations/stories_headings.js
@@ -0,0 +1,101 @@
+
+
+window.headings = {
+  "item-1": {
+    "zh": "舉目見日，不見長安",
+    "en": "With lifted gaze, I see the sun, but not Chang'an"
+  },
+  "item-2": {
+    "zh": "華亭鶴唳",
+    "en": "Cries of cranes back home"
+  },
+   "item-3": {
+    "zh": "道旁苦李",
+    "en": "Plums by the road"
+  },
+  "item-4": {
+    "zh": "問價奴婢",
+    "en": "Price of servants"
+  },
+  "item-5": {
+    "zh": "若如公言，祚安得長",
+    "en": "How can the dynasty last?"
+  },
+  "item-6": {
+    "zh": "沉者自沉，浮者自浮",
+    "en": "Not a postman"
+  },
+  "item-7": {
+    "zh": "以今度之，相當然耳",
+    "en": "Judging from the present"
+  },
+   "item-8": {
+    "zh": "人盡可夫",
+    "en": "Every man can be the husband"
+  },
+   "item-9": {
+    "zh": "肘石有璽",
+    "en": "Stone tied to my elbow"
+  },
+   "item-10": {
+    "zh": "雪若柳絮",
+    "en": "What does the snow resemble?"
+  },
+   "item-11": {
+    "zh": "豈可使卿有勳",
+    "en": "Unnecessary contribution"
+  },
+   "item-12": {
+    "zh": "芳蘭生門",
+    "en": "Clearing Orchids"
+  },
+   "item-13": {
+    "zh": "嵇侍中血",
+    "en": "Do not remove the stains"
+  },
+   "item-14": {
+    "zh": "小子安知壯士志",
+    "en": "How can ordinary people understand the aspirations of heroes?"
+  },
+    "item-15": {
+    "zh": "召客擇人",
+    "en": "Inviting the right guests"
+  },
+    "item-16": {
+    "zh": "請君入甕",
+    "en": "Please enter the urn"
+  },
+    "item-17": {
+    "zh": "流芳後世，遺臭萬載",
+    "en": "Leaving a legacy"
+  },
+    "item-18": {
+    "zh": "囊中取物",
+    "en": "Taking something from the bag"
+  },
+    "item-19": {
+    "zh": "小時了了，大未必佳",
+    "en": "You must have been truly outstanding!"
+  },
+  "item-20": {
+    "zh": "高貴鄉公何在",
+    "en": "Where is the Duke of Gaogui District?"
+  },
+  "item-21": {
+    "zh": "將曹爽我乎",
+    "en": "To be 'Cao Shuang'ed"
+  },
+  "item-22": {
+    "zh": "木猶如此，人何以堪",
+    "en": "The unrelenting passage of time"
+  },
+  "item-23": {
+    "zh": "乘興而行，興盡而返",
+    "en": "All but a whim"
+  },
+  "item-24": {
+    "zh": "汗如出漿，汗不敢出",
+    "en": "To sweat or not to sweat"
+  },
+ 
+};
